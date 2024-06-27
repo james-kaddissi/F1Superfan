c925a543d16d4eaf604593dfa04bf99f14a8978c
ANONyMOSy <jameskaddissi@gmail.com>
2024-06-27 19:08:42 -0400

    Initial Commit

---
+++ F1Superfan/F1SuperfanApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1SuperfanApp.swift
+//  F1Superfan
+//
+//  Created by James Kaddissi on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct F1SuperfanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
